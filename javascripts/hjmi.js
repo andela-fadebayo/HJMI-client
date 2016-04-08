03e702f413d6eb62131f4edaac79df53777e40b4
andela-fadebayo <fiyinfoluwa.adebayo@andela.com>
2016-04-08 23:08:26 +0100

    Add initial data for all other pages.

--- javascripts/hjmi.js
+++ javascripts/hjmi.js
@@ -1,28 +1,44 @@
-// Collapse navbar on scroll
-function collapseNavbar() {
-  if ($(".navbar-hjmi").offset().top > 150) {
-    $(".navbar-fixed-top").addClass("top-nav-collapse");
-  } else {
-    $(".navbar-fixed-top").removeClass("top-nav-collapse");
+var init;
+
+init = function () {
+  var setHomeBannerHeight;
+
+  // Collapse navbar on scroll
+  function collapseNavbar() {
+    if ($(".navbar-hjmi").offset().top > 150) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
   }
-}
+  $(window).scroll(collapseNavbar);
 
-$(window).scroll(collapseNavbar);
-$(document).ready(collapseNavbar);
-
-$(function() {
-  $('a.page-scroll').bind('click', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
+  $(function () {
+    $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+    });
   });
-});
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-  if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
-    $('.navbar-toggle:visible').click();
-  }
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function () {
+    if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+      $('.navbar-toggle:visible').click();
+    }
+  });
+
+  // Set header height
+  // setHomeBannerHeight = function(){
+  //   var homeHeight= $(window).height();
+  //   $('#header').height(homeHeight);
+  // }
+  // setHomeBannerHeight();
+};
+
+
+$(document).ready(function () {
+  init();
 });
